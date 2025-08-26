7a99400a494df949ea9a6c0b004a813ec309f2c4
keshavjagota89-cloud <keshavjagota89@gmail.com>
2025-08-26 17:14:27 +0530

    Add files via upload

---
+++ stFunctions.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+#main title
+st.title("This is the Main title")
+#section heading
+st.header("This is the section heading")
+#sub heading
+st.subheader("This is the subheading")
+
+#Normal paragraph
+st.write("This is my normal para. It's great for description")
+
+st.markdown(
+    """
+    **I am the bold text**, *I am in Italic*,
+    and a [link to Google](https://www.google.com/)
+    """,
+    unsafe_allow_html=True 
+#permission to use html tages in markdown
+)
+
+
