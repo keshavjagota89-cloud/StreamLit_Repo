b925e7ab3a07b59b38c67b52b12697278e37c5c9
keshavjagota89-cloud <keshavjagota89@gmail.com>
2025-08-28 17:18:35 +0530

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
-name = st.text_input("Enter your name")
-birth_year = st.number_input("Your birth year",min_value=1900,max_value=2100,step=1)
+col1,col2 = st.columns(2)
+
+with col1:
+  name = st.text_input("Enter your name")
+with col2: 
+  birth_year = st.number_input("Your birth year",min_value=1900,max_value=2100,step=1)
 
 if name and birth_year:
   current_year = 2025
-  age = st.write(f"Your age is {birth_year - current_year}") 
-  st.write(f"Hello {name}!")
+  age = current_year - birth_year
+  st.write(f"Hello {name}! Your age is {age}")
