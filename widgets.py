3f501bbcabf37b3b2a4d1d70d8338f452c3a4141
keshavjagota89-cloud <keshavjagota89@gmail.com>
2025-08-28 17:22:21 +0530

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -1,13 +1,4 @@
 import streamlit as st
 
-col1,col2 = st.columns(2)
-
-with col1:
-  name = st.text_input("Enter your name")
-with col2: 
-  birth_year = st.number_input("Your birth year",min_value=1900,max_value=2100,step=1)
-
-if name and birth_year:
-  current_year = 2025
-  age = current_year - birth_year
-  st.write(f"Hello {name}! Your age is {age}")
+feedback = st.text_area("Enter your feedback")
+st.write(f"Thank you for the feedback!")
