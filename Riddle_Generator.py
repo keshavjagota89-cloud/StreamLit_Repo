e8d6c8e40518a2a9341a3dfc10997519609f6a07
keshavjagota89-cloud <keshavjagota89@gmail.com>
2025-10-28 17:43:47 +0530

    Create Riddle_Generator.py

---
+++ Riddle_Generator.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import google.generativeai as genai
+
+#Page setup
+st.set_page_config(page_title="AI Riddle Generator")
+st.title("Riddle generator")
+st.write("Get fun riddles for kids! Solve them yourself or click the button to see the answer")
+
+#Gemini setup
+genai.configure(api_key="AIzaSyDwzkmdPpbRX1SOTDkH8UpNTp4zG2lVnNU")
+
+def generate_riddle():
+  """Generates a user-friendly riddle with its answer"""
+  prompt = ("Generate one short, fun, and kid friendly riddle"
+  "Provide the riddle first, and then the answer."
+  "Format it as: Riddle: ... Answer: ...")
+
+  model = genai.GenerativeModel("gemimi-2.5-pro")
+  response = model.generate_content(prompt)
+  return response.text.strip()
+
+  #main app with session state
+# Initialise session state
+if "riddle" not in st.session_state:
+  st.session_state.riddle = ""  
+if "answer" not in st.session_state:
+  st.session_state.answer = "" 
+if "show_answer" not in st.session_state:
+  st.session_state.show_answer = False
+
+  #Generate Riddle button
+if st.button("Generate Riddle"):
+  riddle_text = generate_riddle()#function call  
+  if "Answer:" in riddle_text:
+    riddle1,answer1 = riddle_text.spilt("Answer:",1)
+    st.session_state.riddle = riddle1
+    st.session_state.answer = answer1
+    st.session_state.show_answer = False  
+  else:
+    st.session_state.riddle = riddle_text 
+    st.session_state.answer = ""
+    st.session_state.show_answer = False
+
+     #Display riddle
+if st.session_state.riddle:
+  st.write(st.session_state.riddle)
+
+  #show answer button
+  if st.button("Show Answer"):
+    st.session_state.show_answer = True
+
+#Display answer if requested
+if st.session_state.show_answer  and st.session_state.answer:
+  st.success(st.session_state.answer)
